77c435898f98a8d508c072bdbd56ec8de1d6c7ac
vitoynha <152344440+vitoynha@users.noreply.github.com>
2023-11-28 21:17:59 -0300

    Update Especialista.hpp
    Criação do contrutor e mudação dos nomes

--- Especialista.hpp
+++ Especialista.hpp
@@ -6,34 +6,26 @@
 
 using namespace std;
 
-double perc = 0.1;
-double percWanda = 0.1;
-
-
 class Especialista : public Funcionario {
+  public:
+    string especialidade;
+    int num_atendimentos = 0;
+    double comissao = 0;
 
-    public:
+    Especialista() {}
 
-    public:
-        string especialidade;
-
-    double comissao(double ValorVenda) {
-        double c = ValorVenda*perc;
-                  return c;
+    Especialista(string nome, string idade, int rg_func, double salario_base, string especialidade) {
+      this->salario_base = salario_base; // valor m�nimo recebido pelo funcion�rio
+      this->idade = idade;
+      this->nome = nome;
+      this->rg_func = rg_func;
+      this->especialidade = especialidade;
     }
 
+    double calcularComissao(double ValorVenda);
 
-    void print() {
-
-        std::cout << "[Especialista]" << endl;
-        Funcionario::print();
-
-
-
-        std::cout << "  Nome: " << nome << endl
-        << "  SalarioBase: R$ " << fixed << setprecision(2) << SalarioBase <<endl;
-
-    }
+    /*Fucao que imprime as iunfomacoes dr especialista*/
+    void print();
 };
 
 #endif
